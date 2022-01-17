e41f84809c56f1e9d4071dbfda578298e02fa6c6
Alireza Davari <alireza.davari.h@gmail.com>
2022-01-17 10:49:32 +0330

    add token

--- sqs_queue.py
+++ sqs_queue.py
@@ -8,7 +8,7 @@ from dynamodb_formatter import dict_to_dynamodb
 
 def send_to_output(credentials: Credentials, queue_name: str, rows: list):
     sqs = boto3.resource('sqs', aws_access_key_id=credentials.access_key_id,
-                         aws_secret_access_key=credentials.secret_key)
+                         aws_secret_access_key=credentials.secret_key, aws_session_token=credentials.token)
     queue = sqs.get_queue_by_name(QueueName=queue_name)
     for row in rows:
         queue.send_message(MessageBody=json.dumps(dict_to_dynamodb(row)))
